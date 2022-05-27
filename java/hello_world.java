81eacbb73e8562b4612283e81bb22e1d20bee327
Mamunul Beg <mamun.beg@gmail.com>
2022-05-27 15:54:55 +0100

    init

---
+++ java/hello_world.java
@@ -0,0 +1,7 @@
+// Your First Program
+
+public class hello_world {
+        public static void main(String[] args) {
+                System.out.println("Mamun is introduced to OpenJDK 17!");
+        }
+}
